a060419d3cc1f672d1655ffbb62c1c6b4b094ac8
minyk <minykreva@gmail.com>
2016-08-14 00:02:50 +0900

    Remove other services start scripts.

--- scripts/start-all-services.sh
+++ scripts/start-all-services.sh
@@ -25,61 +25,10 @@
 . /etc/init.d/functions
 
 source /etc/profile.d/java.sh
-source /etc/profile.d/hadoop.sh
-source /etc/profile.d/spark.sh
-source /etc/profile.d/kafka.sh
 source /etc/profile.d/nifi.sh
 
 SVC_USER="root"
 EXEC="runuser -s /bin/bash ${SVC_USER} -c "
-
-function start_hdfs() {
-	$EXEC "$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode"
-	$EXEC "$HADOOP_PREFIX/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode"
-	echo "started hdfs"
-}
-
-function stop_hdfs() {
-	$EXEC "$HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs stop namenode"
-	$EXEC "$HADOOP_PREFIX/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs stop datanode"
-	echo "stopped hdfs"
-}
-
-function start_yarn() {
-	$EXEC "$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager"
-	$EXEC "$HADOOP_YARN_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager"
-	$EXEC "$HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver --config $HADOOP_CONF_DIR"
-	$EXEC "$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver --config $HADOOP_CONF_DIR"
-	echo "started yarn"
-}
-
-function stop_yarn() {
-	$EXEC "$HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR stop resourcemanager"
-	$EXEC "$HADOOP_YARN_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR stop nodemanager"
-	$EXEC "$HADOOP_YARN_HOME/sbin/yarn-daemon.sh stop proxyserver --config $HADOOP_CONF_DIR"
-	$EXEC "$HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh stop historyserver --config $HADOOP_CONF_DIR"
-	echo "stopped yarn"
-}
-
-function start_spark() {
-	$EXEC "$SPARK_HOME/sbin/start-all.sh"
-	$EXEC "$SPARK_HOME/sbin/start-history-server.sh"
-	echo "started spark"
-}
-
-function stop_spark() {
-	$EXEC "$SPARK_HOME/sbin/stop-all.sh"
-	$EXEC "$SPARK_HOME/sbin/stop-history-server.sh"
-	echo "stopped spark"
-}
-
-function start_kafka {
-    $EXEC "$KAFKA_HOME/start-kafka.sh"
-}
-
-function stop_kafka {
-    $EXEC "$KAFKA_HOME/stop-kafka.sh"
-}
 
 function start_nifi {
     $EXEC "$NIFI_HOME/bin/nifi.sh start"
@@ -90,19 +39,11 @@ function stop_nifi {
 }
 
 start() {
-    start_hdfs
-    #start_yarn
-    start_spark
-    start_kafka
     start_nifi
 	return 0
 }
 
 stop() {
-    stop_kafka
-    stop_spark
-    #stop_yarn
-    stop_hdfs
     stop_nifi
 	return 0
 }
