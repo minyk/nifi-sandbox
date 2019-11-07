eb27e85352823edc60381343008b868da45651b8
minyk <minykreva@gmail.com>
2019-11-07 19:22:09 +0900

    Update to nifi 1.10.0

--- scripts/setup-java.sh
+++ scripts/setup-java.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 source "/vagrant/scripts/common.sh"
 
-function installLocalJava {
-	echo "installing oracle jdk"
-	FILE=/vagrant/resources/$JAVA_ARCHIVE
-	tar -xzf $FILE -C /usr/local
-}
-
 function installRemoteJava {
 	echo "install open jdk"
 	yum install -y java-1.8.0-openjdk.x86_64 java-1.8.0-openjdk-devel
-}
-
-function setupJava {
-	echo "setting up java"
-	if resourceExists $JAVA_ARCHIVE; then
-                chown -R root:root /usr/local/${JAVA_HOME}
-		ln -s /usr/local/${JAVA_HOME} /usr/local/java
-	else
-		ln -s /usr/lib/jvm/jre /usr/local/java
-	fi
+	ln -s /usr/lib/jvm/jre /usr/local/java
 }
 
 function setupEnvVars {
@@ -27,15 +12,6 @@ function setupEnvVars {
 	echo JAVA_HOME=/usr/local/java >> /etc/default/java
 }
 
-function installJava {
-	if resourceExists $JAVA_ARCHIVE; then
-		installLocalJava
-	else
-		installRemoteJava
-	fi
-}
-
 echo "setup java"
-installJava
-setupJava
+installRemoteJava
 setupEnvVars
